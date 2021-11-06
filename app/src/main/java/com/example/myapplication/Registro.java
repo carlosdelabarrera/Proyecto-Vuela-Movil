afc3534fc6a9e0d6363600f921264e8e777ff6ee
diegodurancorrea <diegodurancorrea@hotmail.com>
2021-11-05 21:57:17 -0500

    login

---
+++ app/src/main/java/com/example/myapplication/Registro.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+    public void Volver(View view) {
+        Intent intent = new Intent(this, Bienvanida.class);
+        startActivity(intent);
+    }
+}
