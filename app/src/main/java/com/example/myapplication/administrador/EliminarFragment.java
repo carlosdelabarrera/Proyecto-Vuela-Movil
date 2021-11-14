563b9d0ea6ef3c25a89091a5fe023eb82b7369e6
diegodurancorrea <diegodurancorrea@hotmail.com>
2021-11-14 15:22:36 -0500

    AVANCE NAVIGATION DRAWER ADMIN

--- app/src/main/java/com/example/myapplication/administrador/EliminarFragment.java
+++ app/src/main/java/com/example/myapplication/administrador/EliminarFragment.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.administrador;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +9,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.myapplication.AdminSQLiteOpenHelper;
 import com.example.myapplication.R;
 
 /**
@@ -16,7 +22,8 @@ import com.example.myapplication.R;
  * create an instance of this fragment.
  */
 public class EliminarFragment extends Fragment {
-
+    View view;
+    Button button;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -60,7 +67,27 @@ public class EliminarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_eliminar, container, false);
+        button = view.findViewById(R.id.fragment_eliminar_button);
+        button.setOnClickListener(v -> {
+            EditText editText1 = (EditText) view.findViewById(R.id.fragment_eliminar_1);
+            AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(view.getContext(),
+                    "base_datos2", null, 1);
+
+            SQLiteDatabase bd = admin.getWritableDatabase();
+            Integer id_vuelo = Integer.parseInt(editText1.getText().toString());
+            int cant = bd.delete("empresa_de_aviacion", "id_vuelo=" + id_vuelo, null);
+            bd.close();
+            editText1.setText("");
+
+            if (cant == 1)
+                Toast.makeText(view.getContext(), "Se borró el artículo con dicho código",
+                        Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(view.getContext(), "No existe un artículo con dicho código",
+                        Toast.LENGTH_SHORT).show();
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eliminar, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/example/myapplication/administrador/ModificarFragment.java
+++ app/src/main/java/com/example/myapplication/administrador/ModificarFragment.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.administrador;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +9,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.myapplication.AdminSQLiteOpenHelper;
 import com.example.myapplication.R;
 
 /**
@@ -56,11 +62,66 @@ public class ModificarFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+    View view;
+    Button button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modificar, container, false);
+        view= inflater.inflate(R.layout.fragment_modificar, container, false);
+        button= view.findViewById(R.id.fragment_modificar_button);
+        button.setOnClickListener(v -> {
+
+
+            EditText editText1 = (EditText) view.findViewById(R.id.fragment_modificar_1);
+            EditText editText2 = (EditText) view.findViewById(R.id.fragment_modificar_2);
+            EditText editText3 = (EditText) view.findViewById(R.id.fragment_modificar_3);
+            EditText editText4 = (EditText) view.findViewById(R.id.fragment_modificar_4);
+            EditText editText5 = (EditText) view.findViewById(R.id.fragment_modificar_5);
+            EditText editText6 = (EditText) view.findViewById(R.id.fragment_modificar_6);
+            EditText editText7 = (EditText) view.findViewById(R.id.fragment_modificar_7);
+
+
+            AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(view.getContext(), "administracion", null, 1);
+
+
+            SQLiteDatabase bd = admin.getWritableDatabase();
+            Integer id_vuelo = Integer.parseInt(editText1.getText().toString());
+            String ciudad_salida = editText2.getText().toString();
+            String ciudad_llegada = editText3.getText().toString();
+            String fecha_salida= editText4.getText().toString();
+            Integer frecuencia = Integer.parseInt(editText5.getText().toString());
+            Integer precio_ruta = Integer.parseInt(editText6.getText().toString());
+            String tiempo_estimado= editText7.getText().toString();
+
+            ContentValues registro = new ContentValues();
+            registro.put("id_vuelo", id_vuelo);
+            registro.put("ciudad_salida", ciudad_salida);
+            registro.put("ciudad_llegada", ciudad_llegada);
+            registro.put("fecha_salida",fecha_salida);
+            registro.put("frecuencia",frecuencia);
+            registro.put("precio_ruta",precio_ruta);
+            registro.put("tiempo_estimado",tiempo_estimado);
+
+            try {
+                int cant = bd.update("empresa_de_aviacion", registro, "id_vuelo=" + id_vuelo, null);
+                bd.close();
+                Toast.makeText(view.getContext(), "se modificaron los datos"+ String.valueOf(cant), Toast.LENGTH_SHORT)
+                        .show();
+
+            }catch (Exception e){
+                Toast.makeText(view.getContext(), "no existe un artículo con el código ingresado"+ e,
+                            Toast.LENGTH_SHORT).show();
+            }
+
+            //if (cant == 1)
+            //else
+               // Toast.makeText(view.getContext(), "no existe un artículo con el código ingresado",
+                    //    Toast.LENGTH_SHORT).show();
+
+
+        });
+        return view;
     }
 }
